31aa5e7c7b11e4e95b66a480566634957f6006c8
unknown <laxmikantphadke@gmail.com>
2020-10-19 14:45:12 +0530

    fixed

---
+++ precache-manifest.b192bcaa2c7f3d1ecbdbec75e01911e6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "53a29a4c60652f67361f",
+    "url": "/nextweb/static/js/app.9ba7945d.chunk.js"
+  },
+  {
+    "revision": "a29413c8dbd1859e86a4",
+    "url": "/nextweb/static/js/runtime~app.d711da8a.js"
+  },
+  {
+    "revision": "b1a52cef54dd376f69df",
+    "url": "/nextweb/static/js/2.07797775.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/nextweb/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/nextweb/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/nextweb/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nextweb/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/nextweb/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/nextweb/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/nextweb/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/nextweb/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/nextweb/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/nextweb/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/nextweb/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/nextweb/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "54a91b0619ccf9373d525109268219dc",
+    "url": "/nextweb/./fonts/Roboto.ttf"
+  },
+  {
+    "revision": "99fc0816a09395454061301fefa42bf1",
+    "url": "/nextweb/./fonts/Roboto_medium.ttf"
+  },
+  {
+    "revision": "522f327409279d2386a4a7b88a5fc976",
+    "url": "/nextweb/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/nextweb/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/nextweb/expo-service-worker.js"
+  },
+  {
+    "revision": "976bc1351203afba7be1f5791c26e3bc",
+    "url": "/nextweb/register-service-worker.js"
+  },
+  {
+    "revision": "4d30230f5fb873588451568bd54b1165",
+    "url": "/nextweb/static/js/2.07797775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64c5c726c073d0f83185f9dafb92a12",
+    "url": "/nextweb/favicon-16.png"
+  },
+  {
+    "revision": "24ed4672f019214d8a34e3860343cb88",
+    "url": "/nextweb/favicon-32.png"
+  },
+  {
+    "revision": "192b18d0ad6054e7f1db05f4117f5dba",
+    "url": "/nextweb/favicon.ico"
+  },
+  {
+    "revision": "ef8d1e75916ff4ecb7d8103c49a77066",
+    "url": "/nextweb/manifest.json"
+  }
+];
